4d8f156f9cfcf2b83f9037899b0f2a48e17fa4f1
VuVanHuy <huyvv10@fpt.edu.vn>
2026-07-16 12:04:50 +0700

    commit

--- Slot03/src/SortArray.java
+++ Slot03/src/SortArray.java
@@ -121,6 +121,7 @@ public class SortArray {
         }
         System.out.println("");
     }
+    
     public static int countPrime(int a[]){
         int count=0;
         for (int i = 0; i < a.length; i++) {
@@ -128,6 +129,7 @@ public class SortArray {
         }
         return count;
     }
+    
     public static void main(String[] args) {
         int[] arr = {6, 4, 3, 6, 3, 8, 11,9, 7, 9, 5};
         System.out.println("Maximum value: " + findMax(arr));
